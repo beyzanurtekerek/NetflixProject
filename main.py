d152ea1d76f1ffe81cadbf3a49ef09b4df9f69f3
Beyza Nur Tekerek <80324416+beyzanurtekerek@users.noreply.github.com>
2021-05-21 16:21:13 +0300

    new

--- main.py
+++ main.py
@@ -1,89 +1,26 @@
-from smtplib import SMTP   # simple mail transfer protocol
-import gizli
 import speech_recognition as sr
-from gtts import gTTS   # google text to speech
-from playsound import playsound
-import webbrowser
 import time
-import random
-import os
+from komutlar import Komut
 
 r = sr.Recognizer()
-def record(ask = False):
-    with sr.Microphone() as source:
-        if ask:
-            speak(ask)
-        audio = r.listen(source)
-        voice = ''
-        try:
-            voice = r.recognize_google(audio, language = "tr-TR")
-        except sr.UnknownValueError:
-            speak("anlamadım")
-        except sr.RequestError:
-            speak("sistem çalışmıyor")
-        return voice
-
-def response(voice):
-    if 'nasılsın' in voice:
-        speak("merhaba beyza iyiyim")
-    if 'arama' in voice:
-        search = record("ne aramak istiyorsun")
-        url = "https://google.com/search?q=" + search
-        webbrowser.open(url)
-        speak(search + "için bulduklarım")
-    if 'görüşürüz' in voice:
-        speak("görüşürüz")
-    exit()
-
-def speak(string):
-    tts = gTTS(string, lang = "tr")
-    rand = random.randint(1,10000)
-    file = "audio-" + str(rand) + ".mp3"
-    tts.save(file)
-    playsound(file)
-    os.remove(file)
-
-speak("nasıl yardımcı olabilirim beyza")
-time.sleep(1)
-while 1:
-    voice = record()
-    print(voice)
-    response(voice)
-
-# ilk log in olacak
-"""speak("Hoş geldin! Lütfen Netflix'e giriş yap.")
 
 while True:
-    mailAdress = input("Mail adresiniz: ")
-    password = input("Şifreniz: ")
+    with sr.Microphone() as source:
+        print("nasıl yardımcı olabilirim beyza")
+        audio = r.listen(source)
 
-    if mailAdress == "beyzanurtekerk@gmail.com" and password != gizli.sifre:
-        print("Şifre hatalı. Uyarı maili gönderiliyor...")
-        try:
-            # mail bilgileri
-            subject = "Güvenlik uyarısı"
-            message = "Beyza Nur, hesabına hatalı giriş tespit ettik."
-            content = "Subject: {0}\n{1}".format(subject,message)
+    voice = ''
+    try:
+        voice = r.recognize_google(audio, language = "tr-TR")
+        print(voice)
+        komut = Komut(voice)
+        komut.komutBul()
+        time.sleep(1)
 
-            # netflix hesap bilgileri
-            myMailAdress = "netflixproje@gmail.com"
-            myPassword = gizli.sifre
+    except sr.UnknownValueError:
+        print("anlamadım")
 
-            # kime gönderilecek (kullanıcıya)
-            sendTo = "beyzanurtekerk@gmail.com"
-            mail = SMTP("smtp.gmail.com", 587)                            # gmailin host adresini ve kullandıgı hizmet numarasını girdim
-            mail.ehlo()                                                   # baglantı islemini gerceklestirdim
-            mail.starttls()                                               # verileri sifreli bir sekilde göndermek icin
-            mail.login(myMailAdress, myPassword)                          # oturum actık
-            mail.sendmail(myMailAdress, sendTo, content.encode("utf-8"))  # kimden, kime, mesaj
-            print("Güvenlik uyarısı maili gönderildi. Tekrar deneyin.\n--------------------------------------------------")
-        except Exception as i:
-            print("Mail gönderilirken bir hata oluştu! Tekrar deneyin.\n{0}".format(i))
-        continue
-    elif mailAdress == "beyzanurtekerk@gmail.com" and password == gizli.sifre:
-        print("Netflix'e hoş geldin!")
-        break
-    else:
-        print("Böyle bir hesap yok! Tekrar deneyin.\n--------------------------------------------------")
-        continue"""
+    except sr.RequestError:
+        print("sistem çalışmıyor")
+
 
